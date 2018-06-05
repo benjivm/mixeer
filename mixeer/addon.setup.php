367a3b693010876e3fa2c8bf52ead7d66d1b5874
Ben <bvmiller@mail.sfsu.edu>
2018-06-04 21:50:27 -0700

    Move URLs to GitLab

--- mixeer/addon.setup.php
+++ mixeer/addon.setup.php
@@ -7,13 +7,13 @@
  * @category        Plugin
  * @author          Ben Miller
  * @copyright       Copyright (c) 2017, Ben Miller
- * @link            https://github.com/gmask/mixeer
+ * @link            https://gitlab.com/gmask/mixeer
  */
 
 return array(
     'author'      => 'Ben Miller',
     'author_url'  => 'https://ben-miller.com/',
-    'docs_url'    => 'https://github.com/gmask/mixeer',
+    'docs_url'    => 'https://gitlab.com/gmask/mixeer',
     'name'        => 'MixEEr',
     'description' => 'Processes mix manifest files in ExpressionEngine for asset management.',
     'version'     => '1.0',
